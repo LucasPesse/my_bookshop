2e40907d36cea524b4d6af6170951ed9eb6a9fa1
lucaspesse <lucas.pesse@epitech.eu>
2024-11-13 23:20:51 +0100

    Feat: Adding mock page for the book
    Adding google api for book details

---
+++ front/src/components/last_arrival.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let books;
+</script>
+
+<section class="py-12 container mx-auto">
+	<h3 class="text-3xl font-semibold text-center mb-8">Livres en Vedette</h3>
+	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+		{#each books as book}
+			<div class="bg-white rounded-lg shadow-lg overflow-hidden">
+				<img src={book.cover} alt="Couverture de {book.title}" class="w-full h-64 object-cover" />
+				<div class="p-4">
+					<a href={`/${book.id}`} class="text-xl font-bold h-12 overflow-hidden text-ellipsis whitespace-nowrap">
+						{book.title}
+					</a>
+					<p class="text-gray-600">{book.author}</p>
+					<p class="text-blue-600 font-semibold">{book.price}</p>
+					<button class="mt-4 w-full bg-blue-600 py-2 rounded hover:bg-blue-700">
+						Ajouter au Panier
+					</button>
+				</div>
+			</div>
+		{/each}
+	</div>
+</section>
